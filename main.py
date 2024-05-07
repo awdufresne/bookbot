531f8748290f20491e12f467763c7ff302aaa9c2
awdufresne <awdufresne@gmail.com>
2024-05-07 11:24:05 -0500

    iterated on main.py file to count words and letters in a given text file

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+def wordcount(text):
+    words = text.split()
+    #print("Placeholder world count result")
+    return len(words)
+    
+def lettercount(text):
+    lowered_text = text.lower()
+    letters = {}
+    for letter in lowered_text:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        #print(file_contents)
+        total_words = wordcount(file_contents)
+        total_letters = lettercount(file_contents)
+        print(f"Total word count: {total_words}")
+        print(total_letters)
 main()
